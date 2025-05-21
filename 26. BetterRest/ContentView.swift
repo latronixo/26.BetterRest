e4119f70f243f2c824d074817a665a97997bf0fd
Valentin <latronixo@gmail.com>
2025-05-21 21:28:29 +0300

    26. 3/7 Пример работы с компонентом DateComponents() для формирования будильника на 8:00

--- 26. BetterRest/ContentView.swift
+++ 26. BetterRest/ContentView.swift
@@ -17,9 +17,10 @@ struct ContentView: View {
     }
     
     func exampleDates() {
-        let now = Date.now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = now...tomorrow
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? .now 
     }
 }
 
