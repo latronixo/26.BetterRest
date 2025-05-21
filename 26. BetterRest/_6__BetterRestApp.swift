bdc1afee0d6aff0f9a061dd3372deca19fcfdbea
Valentin <latronixo@gmail.com>
2025-05-21 19:07:54 +0300

    Initial Commit

---
+++ 26. BetterRest/_6__BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6__BetterRestApp.swift
+//  26. BetterRest
+//
+//  Created by Валентин on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _6__BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
